64c02d42e7b9788feb5929cec845342994a03f92
Armando Lizarazo <correohstry@gmail.com>
2021-11-01 07:03:17 -0500

    Simply code and delete for

--- numbers.py
+++ numbers.py
@@ -1,19 +1,15 @@
-end = "End Program"
 def run():
-  tabla = int(input("Que tabla quiere consultar? "))
+  tabla = int(input(
+"""
+====================
+Bienvenido al programa de tablas...
+Que tabla quiere consultar? """))
   i = 1
-
   while i <= 10:
       print (str(tabla) +" x " + str(i) + " = " + str(tabla * i))
       i += 1
 
-for i in range(0,5):
-    print ("hola numero de x es ...", i)
-
-
-
-
 if __name__ == "__main__":
     run()  
 
-print(end) 
+print("Eso es todo") 
