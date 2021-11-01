f7d752feefffd01da5f78c3c5b3a183c44adc672
Armando Lizarazo <correohstry@gmail.com>
2021-11-01 17:09:08 -0500

    testing dictionary

--- dicctionary.py
+++ dicctionary.py
@@ -1,7 +1,23 @@
-cargadores {
-  belkinTC (TC: 19000)
-  belkinV8 (V8: 17000)
-}
+crg001 = {'Referencia': 'blkv8', 'Producto': 'Cargador ', 'Marca': 'Belkin ', 'Tipo': 'V8 ', 'Amperaje': '2.1 mah ', 'Precio ': 19000}
+crg002= {'Referencia': 'blktc', 'Producto': 'Cargador ', 'Marca': 'Belkin ', 'Tipo': 'TC ', 'Amperaje': '3.1 mah ', 'Precio ': 17000}
+crg003 = {'Referencia': 'nsuv8', 'Producto': 'Cargador ', 'Marca': 'Naisu ', 'Tipo': 'V8 ', 'Amperaje': '2.4 mah ', 'Precio ': 15000}
+consulta = {input('Digite el codigo a consultar...')}
+if (consulta == crg001):
+  for i in crg001.values():
+    print(i)
+else:
+  for i in crg002.values():
+    print(i)
 
-for i in cargadores():
-  print(i)
+
+
+# if (consulta == "codigo1"):
+#   for i in crg001.values():
+#     print(i)
+#   else:
+#     for i in crg002.values(): 
+#       print(i)
+
+# for r in crg002.values():
+#   return(r)
+  
